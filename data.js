addff39b4ae802d5537643248b382312a07aedd6
Erik Davtyan <erikdavtyan@gmail.com>
2017-04-16 02:12:39 +0400

    change positions to yerevan

--- data.js
+++ data.js
@@ -6,40 +6,40 @@ const Trucks = [
     name: 'First Truck',
     mass: '100 kg',
     time: new Date(2017, 3, 16, 17, 30),
-    positionLat: 44.6327519,
-    positionLang: 42.795402,
+    positionLat: 40.16685104370117,
+    positionLang: 44.44186019897461,
   },
   {
     id: uuid(),
     name: "Second Truck",
     mass: '200 kg',
     time: new Date(2017, 3, 16, 8, 30),
-    positionLat: 42.895401,
-    positionLang: 44.564783
+    positionLat: 40.20738983154297,
+    positionLang: 44.526641845703125
   },
   {
     id: uuid(),
     name: "Third Truck",
     mass: '250 kg',
     time: new Date(2017, 3, 19, 16, 30),
-    positionLat: 43.111112,
-    positionLang: 42.999292
+    positionLat: 40.20637130737305,
+    positionLang: 44.470829010009766
   },
   {
     id: uuid(),
     name: "Forth Truck",
     mass: '300 kg',
     time: new Date(2017, 3, 15, 10, 30),
-    positionLat: 44.123456,
-    positionLang: 42.77777
+    positionLat: 40.167945861816406,
+    positionLang: 44.439117431640625
   },
   {
     id: uuid(),
     name: "Fivth Truck",
     mass: '200 kg',
     time: new Date(2017, 3, 16, 17, 30),
-    positionLat: 43.963201,
-    positionLang: 44.100001
+    positionLat: 40.14704895019531,
+    positionLang: 44.48865509033203
   }
 ];
 
