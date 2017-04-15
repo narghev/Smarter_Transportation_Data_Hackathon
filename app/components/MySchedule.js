da6cfdf2cbad42c7f89659382f17cb9abf88116e
Erik Davtyan <erikdavtyan@gmail.com>
2017-04-15 23:44:34 +0400

    add text field

--- app/components/MySchedule.js
+++ app/components/MySchedule.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 import Divider from 'material-ui/Divider';
+import TextField from 'material-ui/TextField';
 import {observer} from 'mobx-react';
 
 export default
@@ -10,14 +11,14 @@ class MySchedule extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-
+      newTruckName: "",
     };
   };
   render() {
     const RenderMyTrucks = () => {
       return (
         <Menu
-          style={{width: "20%"}}
+          style={{width: "15%"}}
         >
           {this.props.myTrucks.map(truck => {
             return (
@@ -29,6 +30,12 @@ class MySchedule extends React.PureComponent {
               </div>
             );
           })}
+          <TextField
+            style={{postition: "fixed", bottom: "0"}}
+            floatingLabelText="Add New Truck"
+            name="New Truck"
+            value={this.state.newTruckName}
+          />
         </Menu>
       );
     };
