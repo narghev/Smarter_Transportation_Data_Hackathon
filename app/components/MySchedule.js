5677bea86528b43b787ba3e23172efe10d65a11b
Erik Davtyan <erikdavtyan@gmail.com>
2017-04-15 23:57:10 +0400

    change the global schedule array

--- app/components/MySchedule.js
+++ app/components/MySchedule.js
@@ -3,6 +3,8 @@ import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 import Divider from 'material-ui/Divider';
 import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import uuid from 'uuid/v4';
 import {observer} from 'mobx-react';
 
 export default
@@ -15,10 +17,11 @@ class MySchedule extends React.PureComponent {
     };
   };
   render() {
+    console.log(this.props.myTrucks)
     const RenderMyTrucks = () => {
       return (
         <Menu
-          style={{width: "15%"}}
+          style={{width: "20%"}}
         >
           {this.props.myTrucks.map(truck => {
             return (
@@ -30,18 +33,34 @@ class MySchedule extends React.PureComponent {
               </div>
             );
           })}
-          <TextField
-            style={{postition: "fixed", bottom: "0"}}
-            floatingLabelText="Add New Truck"
-            name="New Truck"
-            value={this.state.newTruckName}
-          />
         </Menu>
       );
     };
     return (
       <div>
         <RenderMyTrucks />
+        <TextField
+          style={{postition: "fixed", bottom: "0", width: "15%"}}
+          floatingLabelText="Add New Truck"
+          name="New Truck"
+          value={this.state.newTruckName}
+          onChange={(e) => this.setState({newTruckName: e.target.value})}
+        />
+        <FlatButton
+          label="save"
+          style={{width: "5%"}}
+          onTouchTap={() => {
+            this.props.myTrucks.push({
+              id: uuid(),
+              name: this.state.newTruckName,
+              mass: '100 kg',
+              time: new Date(),
+              positionLat: 43.6327519,
+              positionLang: 44.795402,
+            });
+            this.setState({newTruckName: ""});
+        }}
+        />
       </div>
     );
   };
