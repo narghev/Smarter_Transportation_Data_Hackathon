c5134cc9ae410d3dcef63a64d85cf515f9e62e0c
Erik Davtyan <erikdavtyan@gmail.com>
2017-04-16 02:02:59 +0400

    render markers on map

--- app/components/AllTrucks.js
+++ app/components/AllTrucks.js
@@ -1,18 +1,7 @@
 import _ from 'lodash';
 import {observer} from 'mobx-react';
-
-import {
-  default as React,
-  Component,
-} from 'react';
-
-
-import {
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from 'react-google-maps';
-
+import {default as React, Component} from 'react';
+import {withGoogleMap, GoogleMap, Marker} from 'react-google-maps';
 
 const Map = withGoogleMap(props => (
   <GoogleMap
@@ -36,42 +25,19 @@ class MapContainer extends Component {
   constructor(props){
     super(props);
     this.state = {
-      markers: [
-        {
-        position: {
-          lat: 40.184501,
-          lng: 44.514995,
-        },
-          key: `Kentron`,
-          defaultAnimation: 1,
-        }
-      ],
+      markers: [],
     };
   };
 
-  handleMapLoad = (map) => {
-    this._mapComponent = map;
-    if (map) {
-      console.log(map.getZoom());
-    }
-  };
-
-
-  handleMapClick = (event) => {
-    const nextMarkers = [
-      ...this.state.markers,
-      {
-        position: event.latLng,
-        defaultAnimation: 2,
-        key: Date.now(),
-      },
-    ];
-    this.setState({markers: nextMarkers});
-  };
-
-  handleMarkerRightClick = (targetMarker) => {
-    const nextMarkers = this.state.markers.filter(marker => marker !== targetMarker);
-    this.setState({markers: nextMarkers});
+  componentWillMount() {
+    const markers = this.props.allTrucks.map(truck => {
+      return {
+        position: {lat: truck.positionLat, lng: truck.positionLang},
+        key: truck.id,
+        defaultAnimation: 1
+      }
+    });
+    this.setState({markers});
   };
 
   render() {
@@ -84,10 +50,7 @@ class MapContainer extends Component {
           mapElement={
             <div style={{ height: `100vh` }} />
           }
-          onMapLoad={this.handleMapLoad}
-          onMapClick={this.handleMapClick}
           markers={this.state.markers}
-          onMarkerRightClick={this.handleMarkerRightClick}
         />
       </div>
     );
